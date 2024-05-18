f32154570b4dd7bcd7dd89966bcfecabbdc74f7b
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-05-18 14:44:03 -0400

    finished log implementation for camera script

--- src/main.py
+++ src/main.py
@@ -2,8 +2,25 @@ import os
 import time
 import cv2
 import schedule
+import logging
 import numpy as np
 from datetime import datetime, timedelta, date
+
+
+def log_print(msg, level=logging.INFO):
+    global print_logs_msgs
+
+    if print_logs_msgs:
+        print("{}: {}".format(datetime.now(), msg))
+
+    if level == logging.WARNING:
+        logging.warning(msg)
+    elif level == logging.ERROR:
+        logging.error(msg)
+    elif level == logging.CRITICAL:
+        logging.critical(msg)
+    else:
+        logging.info(msg)
 
 
 def get_current_hour_str():
@@ -19,15 +36,18 @@ def get_current_hour_str():
 
 def setup_directories():
     global data_dir, current_dir
+
     today = date.today()
     new_path = "{}/{}/{}/{}".format(data_dir, today.year, today.month, today.day)
+
     if not os.path.exists(new_path):
-        print("{}: creating new directory for {}".format(datetime.now(), today))
+        log_print("creating new directory for {}".format(today))
         os.makedirs(new_path)
     else:
-        print("{}: directory already exists for {}".format(datetime.now(), today))
+        log_print("directory already exists for {}".format(today))
+
     current_dir = new_path
-    print("{}: current file directory set as \"{}\"".format(datetime.now(), current_dir))
+    log_print("current file directory set as \"{}\"".format(current_dir))
 
 
 def capture_routine():
@@ -40,9 +60,14 @@ def capture_routine():
     cam = cv2.VideoCapture(0)
     if not use_webcam:
         cam = cv2.VideoCapture(camera_url)
-        print("{}: connecting to rtsp camera".format(datetime.now()))
+        log_print("initializing rtsp camera device")
     else:
-        print("{}: connecting to default camera (webcam)".format(datetime.now()))
+        log_print("initializing default camera (webcam) device")
+
+    # Check if camera is operational
+    if not cam.isOpened():
+        log_print("camera not open, cannot extract feed, exiting routine", logging.ERROR)
+        return
 
     # Initialize naming variables
     cam_window_name = "rtsp camera feed"
@@ -60,7 +85,7 @@ def capture_routine():
         # Grab frame from camera
         success, frame = cam.read()
         if not success:
-            print("{}: failed to grab frame".format(datetime.now()))
+            log_print("failed to grab frame from camera feed, ending capture routine", logging.ERROR)
             break
 
         # Show current feed if show_window is True
@@ -71,24 +96,20 @@ def capture_routine():
         if datetime.now() >= next_photo_time:
             img_name = "{}_{}.png".format(base_filename, img_counter+1)
             cv2.imwrite(img_name, frame)
-            print("{}: snapshot taken, saved as \"{}\"".format(datetime.now(), img_name))
+            log_print("snapshot taken, saved as \"{}\"".format(img_name))
             next_photo_time = datetime.now() + timedelta(seconds=photo_interval)
             img_counter += 1
 
-        # Stop condition(s)
-        if img_counter == max_images:
-            print("{}: successfully captured 3 images during live block".format(datetime.now()))
-            break
-
-        if datetime.now() >= end_time:
-            print("{}: camera live block has ended".format(datetime.now()))
+        # Stop conditions
+        if img_counter == max_images or datetime.now() >= end_time:
+            log_print("concluding capture routine for current live block, captured {} images".format(img_counter))
             break
 
         # Wait interval until loop is repeated (100ms = ~ 10fps)
         cv2.waitKey(100)
 
     # Garbage collection
-    print("{}: closing connection to camera".format(datetime.now()))
+    log_print("closing connection to camera")
     cam.release()
     cv2.waitKey(1000)
     cv2.destroyAllWindows()
@@ -97,13 +118,11 @@ def capture_routine():
 
 if __name__ == '__main__':
 
-    print("{}: starting capture script".format(datetime.now()))
-
     # Script Configuration
-    use_webcam = False          # for local debugging
-    show_window = False         # for local debugging
-    log_dir = "./../logs"       # log files location
-    data_dir = "./../data"      # base img file location
+    use_webcam = False          # controls whether webcam is used instead of 5G camera (debugging)
+    show_window = False         # controls whether live stream is shown during capture routine (debugging)
+    print_logs_msgs = True      # controls whether log msgs are printed to console (debugging)
+    data_dir = "./data"         # base img file location
     current_dir = ""            # global variable for where img files are currently being saved
     live_duration = 5           # minutes during which the camera is live (5 minutes)
     photo_interval = 90         # seconds between each photograph during a live block (90 seconds)
@@ -112,7 +131,6 @@ if __name__ == '__main__':
     ip_address = "174.90.198.126"
     rtsp_port = "554"
     camera_url = "rtsp://{}:{}/main".format(ip_address, rtsp_port)
-    print("{}: rtsp camera url set to \"{}\"".format(datetime.now(), camera_url))
 
     # Schedule Configuration
     schedule.every().day.at("06:00").do(capture_routine)      # 6am
@@ -128,12 +146,20 @@ if __name__ == '__main__':
     schedule.every().day.at("16:00").do(capture_routine)      # 4pm
     schedule.every().day.at("17:00").do(capture_routine)      # 5pm
     schedule.every().day.at("18:00").do(capture_routine)      # 6pm
-
     # schedule.every(90).seconds.do(capture_routine)  # debugging
 
-    print("{}: scheduled jobs:".format(datetime.now()))
+    # Log initialization
+    log_file = "./capture_log.log"
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    log_date_fmt = '%d-%b-%y %H:%M:%S'
+    logging.basicConfig(filename=log_file, format=log_format, datefmt=log_date_fmt, level=logging.INFO)
+
+    log_print("starting capture script...")
+    log_print("rtsp camera url set to \"{}\"".format(camera_url))
+    jobs_str = "{} scheduled jobs:".format(len(schedule.jobs))
     for job in schedule.jobs:
-        print("\t{}".format(job))
+        jobs_str = jobs_str + ("\n\t{}".format(job))
+    log_print(jobs_str)
 
     while True:
         schedule.run_pending()
