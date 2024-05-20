8dcdad24292c5aa4dce94f2bdf80de2765551c26
Nicholas Strzelczyk <17nas6@queensu.ca>
2024-05-20 16:28:03 -0400

    reconfigured how log was implemented, started email implementation but not finished

--- src/data_usage_calculator.py
+++ src/data_usage_calculator.py
@@ -1,37 +1,50 @@
 
-if __name__ == '__main__':
+#########################################################
+#                                                       #
+#   THIS CODE IS UNFINISHED AND UNTESTED FOR ACCURACY   #
+#                                                       #
+#########################################################
 
+def calc_estimates(pixels_w, pixels_h, fps, intervals, interval_len):
+	global MiB_conv
+
+	live_minutes_per_day = float(intervals * interval_len)
+	frames_per_day = float(fps * live_minutes_per_day * 60)
+
+	total_pixels_per_frame = float(pixels_w * pixels_h)
+	total_megaBYTES_per_frame = float((total_pixels_per_frame * 3) / 1_000_000)
+
+	daily_MiB_estimate = float(MiB_conv * total_megaBYTES_per_frame * frames_per_day)
+	weekly_MiB_estimate = daily_MiB_estimate * 7.0
+	monthly_MiB_estimate = weekly_MiB_estimate * 4.0
+
+	estimates = {
+		'd': daily_MiB_estimate,
+		'w': weekly_MiB_estimate,
+		'm': monthly_MiB_estimate
+	}
+	return estimates
+
+def print_estimates(estimates, fps):
+	print("{} fps Daily Estimate: {:,.2f} MiB".format(fps, estimates['d']))
+	print("{} fps Weekly Estimate: {:,.2f} MiB".format(fps, estimates['w']))
+	print("{} fps Monthly Estimate: {:,.2f} MiB".format(fps, estimates['m']))
+	print("")
+
+
+if __name__ == '__main__':
+	MiB_conv = 1.048576 # 1 MiB = this many MB (global constant)
+
+	# -- Variables for estimates --
 	img_height = 576            # pixels
 	img_width = 704             # pixels
-	bit_rate = 2048             # kbps
-	fps = 25                    # fps
-	daily_rec_intervals = 6     # live blocks scheduled per day
+	fps_list = [25, 15, 10]     # fps
+	daily_intervals = 13        # live blocks scheduled per day
 	length_of_intervals = 10    # minutes
 
 	# ------------------------- #
 
-	MiB_conv = 1.048576         # 1 MiB = this many MB
-	live_minutes_per_day = float(daily_rec_intervals * length_of_intervals)
-	frames_per_day = float(fps * live_minutes_per_day * 60)
-
-	total_pixels = float(img_height * img_width)
-	total_megaBYTES = float((total_pixels * 3) / 1_000_000)
-	uncompressed_daily_MiB_estimate = float(MiB_conv * total_megaBYTES * frames_per_day)
-	uncompressed_weekly_MiB_estimate = uncompressed_daily_MiB_estimate * 7.0
-	uncompressed_monthly_MiB_estimate = uncompressed_weekly_MiB_estimate * 4.0
-
-	kiloBITS_per_day = float(bit_rate * live_minutes_per_day * 60)
-	megaBYTES_per_day = float(kiloBITS_per_day / 8_000_000)
-	compressed_daily_MiB_estimate = float(MiB_conv * megaBYTES_per_day)
-	compressed_weekly_MiB_estimate = compressed_daily_MiB_estimate * 7.0
-	compressed_monthly_MiB_estimate = compressed_weekly_MiB_estimate * 4.0
+	for fps in fps_list:
+		print_estimates(calc_estimates(img_width, img_height, fps, daily_intervals, length_of_intervals), fps)
 
 	# ------------------------- #
-
-	print("Daily (uncompressed): {} MiB".format(uncompressed_daily_MiB_estimate))
-	print("Weekly (uncompressed): {} MiB".format(uncompressed_weekly_MiB_estimate))
-	print("Monthly (uncompressed): {} MiB".format(uncompressed_monthly_MiB_estimate))
-	print("")
-	print("Daily (compressed): {} MiB".format(compressed_daily_MiB_estimate))
-	print("Weekly (compressed): {} MiB".format(compressed_weekly_MiB_estimate))
-	print("Monthly (compressed): {} MiB".format(compressed_monthly_MiB_estimate))
--- src/main.py
+++ src/main.py
@@ -1,26 +1,18 @@
 import os
+import sys
 import time
 import cv2
 import schedule
 import logging
+import atexit
+import smtplib
 import numpy as np
 from datetime import datetime, timedelta, date
+from email.message import EmailMessage
 
 
-def log_print(msg, level=logging.INFO):
-    global print_logs_msgs
-
-    if print_logs_msgs:
-        print("{}: {}".format(datetime.now(), msg))
-
-    if level == logging.WARNING:
-        logging.warning(msg)
-    elif level == logging.ERROR:
-        logging.error(msg)
-    elif level == logging.CRITICAL:
-        logging.critical(msg)
-    else:
-        logging.info(msg)
+def log(msg, level=logging.INFO):
+    logging.log(level, msg)
 
 
 def get_current_hour_str():
@@ -37,21 +29,25 @@ def get_current_hour_str():
 def setup_directories():
     global data_dir, current_dir
 
+    # Determine the date and construct path string
     today = date.today()
     new_path = "{}/{}/{}/{}".format(data_dir, today.year, today.month, today.day)
 
+    # Check if the constructed path exists and create it if false
     if not os.path.exists(new_path):
-        log_print("creating new directory for {}".format(today))
+        log("creating new directory for {}".format(today))
         os.makedirs(new_path)
     else:
-        log_print("directory already exists for {}".format(today))
+        log("directory already exists for {}".format(today))
 
+    # Set global variable and log changes
     current_dir = new_path
-    log_print("current file directory set as \"{}\"".format(current_dir))
+    log("current file directory set as \"{}\"".format(current_dir))
 
 
 def capture_routine():
     global use_webcam, show_window, camera_url, current_dir, live_duration, photo_interval
+    log("starting capture routine")
 
     # Check if directories have been made
     setup_directories()
@@ -60,13 +56,13 @@ def capture_routine():
     cam = cv2.VideoCapture(0)
     if not use_webcam:
         cam = cv2.VideoCapture(camera_url)
-        log_print("initializing rtsp camera device")
+        log("initializing rtsp camera device")
     else:
-        log_print("initializing default camera (webcam) device")
+        log("initializing default camera (webcam) device")
 
     # Check if camera is operational
     if not cam.isOpened():
-        log_print("camera not open, cannot extract feed, exiting routine", logging.ERROR)
+        log("camera not open, cannot extract feed, exiting routine", logging.ERROR)
         return
 
     # Initialize naming variables
@@ -85,7 +81,7 @@ def capture_routine():
         # Grab frame from camera
         success, frame = cam.read()
         if not success:
-            log_print("failed to grab frame from camera feed, ending capture routine", logging.ERROR)
+            log("failed to grab frame from camera feed, ending capture routine", logging.ERROR)
             break
 
         # Show current feed if show_window is True
@@ -96,36 +92,72 @@ def capture_routine():
         if datetime.now() >= next_photo_time:
             img_name = "{}_{}.png".format(base_filename, img_counter+1)
             cv2.imwrite(img_name, frame)
-            log_print("snapshot taken, saved as \"{}\"".format(img_name))
+            log("snapshot taken, saved as \"{}\"".format(img_name))
             next_photo_time = datetime.now() + timedelta(seconds=photo_interval)
             img_counter += 1
 
         # Stop conditions
         if img_counter == max_images or datetime.now() >= end_time:
-            log_print("concluding capture routine for current live block, captured {} images".format(img_counter))
+            log("stop condition met, concluding capture routine")
             break
 
         # Wait interval until loop is repeated (100ms = ~ 10fps)
         cv2.waitKey(100)
 
+    # Summarize Results
+    log("captured {} images during {} block".format(img_counter, max_images))
+
     # Garbage collection
-    log_print("closing connection to camera")
+    log("closing connection to camera")
     cam.release()
     cv2.waitKey(1000)
     cv2.destroyAllWindows()
     cv2.waitKey(5000)
+    log("capture routine has concluded")
+
+
+# SENDING EMAILS DOES NOT WORK - May 20th, 2024
+# Perhaps turn this into another handler for the log
+def send_email(msg_body, severity):
+    recipients = ["nstrzel@uwo.ca", "sgomezro@uwo.ca"]
+    default_preamble = (
+        "Ignore this email if this was a test or on purpose.\n"
+        "Check the logs for more information.\n"
+        "Details:\n"
+    )
+
+    # Construct the msg
+    msg = EmailMessage()
+    msg["Subject"] = "{} Severity - Cooling Tower Capture Script Notification".format(severity)
+    msg['From'] = "nstrzel@uwo.ca"
+    msg['To'] = "nstrzel@uwo.ca"    # FOR TESTING
+    # msg['To'] = ", ".join(recipients)
+    msg.preamble = default_preamble + "\n\n" + msg_body + "\n\n"
+
+    # Send the msg
+    server = smtplib.SMTP('localhost')
+    server.send_message(msg)
+    server.quit()
+    log("notification email was sent")
+
+
+def exit_handler():
+    log("ending script...\n##################################################", logging.CRITICAL)
+    return
 
 
 if __name__ == '__main__':
 
     # Script Configuration
-    use_webcam = False          # controls whether webcam is used instead of 5G camera (debugging)
-    show_window = False         # controls whether live stream is shown during capture routine (debugging)
-    print_logs_msgs = True      # controls whether log msgs are printed to console (debugging)
-    data_dir = "./data"         # base img file location
-    current_dir = ""            # global variable for where img files are currently being saved
-    live_duration = 5           # minutes during which the camera is live (5 minutes)
-    photo_interval = 90         # seconds between each photograph during a live block (90 seconds)
+    use_webcam = True              # controls whether webcam is used instead of 5G camera (debugging)
+    show_window = True             # controls whether live stream is shown during capture routine (debugging)
+    verbose = True                 # controls whether log msgs are printed to console (debugging)
+    send_emails = False              # controls whether notification emails are sent in specific situations
+    data_dir = "./data"             # base img file location
+    current_dir = ""                # global variable for where img files are currently being saved
+    live_duration = 5               # minutes during which the camera is live (5 minutes)
+    photo_interval = 90             # seconds between each photograph during a live block (90 seconds)
+    atexit.register(exit_handler)   # runs a routine to notify us if the script exits
 
     # Camera Connection Configuration
     ip_address = "174.90.198.126"
@@ -149,17 +181,41 @@ if __name__ == '__main__':
     # schedule.every(90).seconds.do(capture_routine)  # debugging
 
     # Log initialization
-    log_file = "./capture_log.log"
-    log_format = "%(asctime)s - %(levelname)s - %(message)s"
-    log_date_fmt = '%d-%b-%y %H:%M:%S'
-    logging.basicConfig(filename=log_file, format=log_format, datefmt=log_date_fmt, level=logging.INFO)
+    logging.getLogger().setLevel(logging.INFO)
+    logging.captureWarnings(True)
+    log_formatter = logging.Formatter(fmt="%(asctime)s [%(levelname)s] - %(message)s", datefmt="%d-%b-%y %H:%M:%S")
 
-    log_print("starting capture script...")
-    log_print("rtsp camera url set to \"{}\"".format(camera_url))
-    jobs_str = "{} scheduled jobs:".format(len(schedule.jobs))
-    for job in schedule.jobs:
-        jobs_str = jobs_str + ("\n\t{}".format(job))
-    log_print(jobs_str)
+    # Log file handler initialization
+    file_handler = logging.FileHandler("./capture_log.log")
+    file_handler.setFormatter(log_formatter)
+    logging.getLogger().addHandler(file_handler)
+
+    # Optional log console handler initialization
+    if verbose:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(log_formatter)
+        logging.getLogger().addHandler(console_handler)
+
+    # Optional log email handler initialization
+    # if send_emails:
+    #     email_handler = logging.FileHandler()
+    #     email_handler.setFormatter(log_formatter)
+    #     email_handler.setLevel(logging.ERROR)
+    #     logging.getLogger().addHandler(email_handler)
+
+    # Begin script
+    log("starting script...")
+    summary_str = "script variables:"
+    summary_str = summary_str + "\n\tuse_webcam = {}".format(use_webcam)
+    summary_str = summary_str + "\n\tshow_window = {}".format(show_window)
+    summary_str = summary_str + "\n\tverbose = {}".format(verbose)
+    summary_str = summary_str + "\n\tsend_emails = {}".format(send_emails)
+    summary_str = summary_str + "\n\tdata_directory = \"{}\"".format(data_dir)
+    summary_str = summary_str + "\n\tlive_duration = {} minutes".format(live_duration)
+    summary_str = summary_str + "\n\tphoto_interval = {} seconds".format(photo_interval)
+    summary_str = summary_str + "\n\tcamera_url = \"{}\"".format(camera_url)
+    summary_str = summary_str + "\n\tnum_of_scheduled_jobs = {}".format(len(schedule.jobs))
+    log(summary_str)
 
     while True:
         schedule.run_pending()
